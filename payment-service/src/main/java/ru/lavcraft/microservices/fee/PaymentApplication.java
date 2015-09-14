3b0ca88c4a6b09cb49d1f06fa229e6050d4a1b78
Kirill Tolkachev <lavcraft@gmail.com>
2015-09-14 09:57:12 +0300

    Add payment service

---
+++ payment-service/src/main/java/ru/lavcraft/microservices/fee/PaymentApplication.java
@@ -0,0 +1,75 @@
+package ru.lavcraft.microservices.fee;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+/**
+ * @author tolkv
+ * @since 13/09/15
+ */
+@EnableFeignClients
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentApplication.class, args);
+  }
+}
+
+@RestController
+@RequestMapping("/payments")
+class PaymentController {
+
+  public static final String ACCESS_FULL = "FULL";
+
+  @RequestMapping(value = "/check", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity getPaymetnPermission(@RequestParam Long userId, @RequestParam Optional<OperationType> operationType) {
+    if (userId == 100)
+      throw new RuntimeException("UserIsBlocked");
+
+    return ResponseEntity.ok(Response.builder()
+        .additionalFee(Response.Fee.builder()
+            .max(1000)
+            .min(100)
+            .build())
+        .access(ACCESS_FULL)
+        .operationType(operationType.orElse(OperationType.EXTERNAL))
+        .build());
+  }
+}
+
+@Data
+@Builder
+class Response {
+  private Fee additionalFee;
+  private OperationType operationType;
+  private String access;
+
+  @Data
+  @Builder
+  static class Fee {
+    int max;
+    int min;
+  }
+}
+
+enum OperationType {
+  EXTERNAL,
+  INTERNAL,
+  CUSTOM,
+  INTERNATIONAL
+}
